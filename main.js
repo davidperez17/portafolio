88e3766c144c801ed9fd1cc01ad8b66be3b9d242
davidperez17 <archlpgg7@gmail.com>
2022-04-04 11:59:52 -0600

    se agrego el navbar

---
+++ main.js
@@ -0,0 +1,19 @@
+const btnScrollToTop = document.querySelector("#btn__scroll");
+const hamburguesa = document.querySelector(".hamburguesa");
+const navList = document.querySelector(".lista");
+
+btnScrollToTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+hamburguesa.addEventListener("click", function () {
+  this.classList.toggle("is-active");
+});
+
+navList.addEventListener("click", function () {
+  this.classList.toggle("is-active");
+});
