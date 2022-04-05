7a09a8b12b43407737f79579fc8e8f8ad68fdeda
davidperez17 <archlpgg7@gmail.com>
2022-04-05 00:01:32 -0600

    se agrego el efecto parallax

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const btnScrollToTop = document.querySelector("#btn__scroll");
 const hamburguesa = document.querySelector(".hamburguesa");
 const navList = document.querySelector(".lista");
 
+const background = document.querySelector(".hoja__rota");
+
 btnScrollToTop.addEventListener("click", function () {
   window.scrollTo({
     top: 0,
@@ -20,4 +22,9 @@ navList.addEventListener("click", function () {
 
 hamburguesa.addEventListener("click", () => {
   navList.classList.toggle("active");
+});
+
+window.addEventListener("scroll", () => {
+  const scroll = window.pageYOffset;
+  background.style.top = scroll / -2.5 + "px";
 });
